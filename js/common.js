b770eeab976464f589f3091b426cea924d1102ad
mori <k.andar.m.p@gmail.com>
2019-04-25 17:08:54 +0900

    棒グラフ作成

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
 (function(){
+  var scrollAnimationElm = document.querySelectorAll('.graph');
+  var scrollAnimationFunc = function() {
+    for(var i = 0; i < scrollAnimationElm.length; i++) {
+      if (window.innerHeight < 820){
+        var triggerMargin = 100;
+      }else{
+        var triggerMargin = 300;
+      }
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+
   $('.skill_tab li').click(function(){
     var this_tab = $(this).attr("class");
 
@@ -6,7 +22,8 @@
     $('.skill_tab .' + this_tab +' span').addClass("on");
 
     $('.skill_box_out .skill-box').css('display','none');
+    $('.skill_box_out .skill-box .graph').removeClass("on");
     $('.skill_box_out .' + this_tab).css('display','block');
+    scrollAnimationFunc();
   })
-
 })();
